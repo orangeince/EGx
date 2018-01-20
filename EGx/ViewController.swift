9d35a76406c6bc577ba00df9c78c8350ec02db2d
少龙 <orangeince@outlook.com>
2018-01-20 16:34:56 +0800

    Initial Commit

---
+++ EGx/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EGx
+//
+//  Created by 少 on 2018/1/20.
+//  Copyright © 2018年 少. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
